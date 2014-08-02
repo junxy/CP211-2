4c9638b95e7a06c85364050deb2a0fce85142470
Junior Hsu <ayijun@gmail.com>
2014-08-02 15:39:05 +0800

    Added: Created Model folder and Model Group Created Card model class

---
+++ Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by xu yijun on 8/2/14.
+//  Copyright (c) 2014 Junior xu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by xu yijun on 8/2/14.
+//  Copyright (c) 2014 Junior xu. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
